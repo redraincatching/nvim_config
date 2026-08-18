86f8734309a5f755ae266716a8d2d4f8d6d5682b
éidhne <redraincatching@disroot.org>
2026-08-18 14:02:10 +0100

    lazygit

--- lua/redraincatching/core/lazy.lua
+++ lua/redraincatching/core/lazy.lua
@@ -117,5 +117,25 @@ require('lazy').setup({
     -- git stuff
     { "tpope/vim-fugitive" },
     { "lewis6991/gitsigns.nvim" },
-    { "sindrets/diffview.nvim" }
+    { "sindrets/diffview.nvim" },
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
+    }
 })
